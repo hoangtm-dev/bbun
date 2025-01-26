a9c4fe91376e96aef0e0a1d788a713374e623e71
Hoàng <hoangtm.pro@gmail.com>
2025-01-26 19:06:50 +0700

    Update script.js

--- background/happy/script.js
+++ background/happy/script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'Phương Thúy' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!','Phương Thúy' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
